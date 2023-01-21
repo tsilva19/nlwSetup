6de693f5e7e8a494ed8517f3680ff5b592137b19
Thiago <thiagoalves060@gmail.com>
2023-01-21 13:36:39 -0300

    rotas do servidor

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,15 @@
 // Back-end API REstful
 import Fastify from "fastify";
-import { PrismaClient } from  '@prisma/client';
+
 import cors from '@fastify/cors';
+import { appRoutes } from "./routes";
 
 const app = Fastify()
-const prisma = new PrismaClient()
+
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async() =>{
- const habits = await prisma.habit.findMany();
-
- return habits
-})
 
 app.listen({
  port: 3333,
