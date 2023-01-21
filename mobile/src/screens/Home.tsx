2c57b66fb23a7d3bfa2903bb9d45acd672fb741f
Thiago <thiagoalves060@gmail.com>
2023-01-21 20:57:44 -0300

    mobile alteracao

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -2,7 +2,7 @@ import { View, Text, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
-
+import "../lib/dayjs.ts";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S','S'];
 
@@ -48,6 +48,7 @@ export function Home(){
       .from({ length: amountOfDaysToFill})
       .map((_, i) =>(
         <View
+        key={i}
         className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40'
         style={{ width: DAY_SIZE, height: DAY_SIZE}}
         >
